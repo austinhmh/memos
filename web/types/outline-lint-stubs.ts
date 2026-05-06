56dbe24d670d9adb486de46a38e8e71e70a47c29
huangminghui <huangminghui@xiaomi.com>
2026-05-06 16:33:21 +0800

    fix: stabilize backup restore frontend lint

---
+++ web/types/outline-lint-stubs.ts
@@ -0,0 +1,52 @@
+const value: any = undefined as any;
+export default value;
+export const EditorStyleHelper: any = value;
+export type EditorStyleHelper = any;
+export const createEditorTheme: any = value;
+export type createEditorTheme = any;
+export const enterInCode: any = value;
+export type enterInCode = any;
+export const findParentNode: any = value;
+export type findParentNode = any;
+export const getCurrentBlock: any = value;
+export type getCurrentBlock = any;
+export const indentInCode: any = value;
+export type indentInCode = any;
+export const isCode: any = value;
+export type isCode = any;
+export const isInCode: any = value;
+export type isInCode = any;
+export const isMac: any = value;
+export type isMac = any;
+export const isMermaid: any = value;
+export type isMermaid = any;
+export const listWrappingInputRule: any = value;
+export type listWrappingInputRule = any;
+export const markInputRuleForPattern: any = value;
+export type markInputRuleForPattern = any;
+export const moveToNextNewline: any = value;
+export type moveToNextNewline = any;
+export const moveToPreviousNewline: any = value;
+export type moveToPreviousNewline = any;
+export const newlineInCode: any = value;
+export type newlineInCode = any;
+export const openLink: any = value;
+export type openLink = any;
+export const outdentInCode: any = value;
+export type outdentInCode = any;
+export const pluginKey: any = value;
+export type pluginKey = any;
+export const richUIExtensions: any = value;
+export type richUIExtensions = any;
+export const selectAll: any = value;
+export type selectAll = any;
+export const toggleCheckboxItems: any = value;
+export type toggleCheckboxItems = any;
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "relative-time": any;
+    }
+  }
+}
